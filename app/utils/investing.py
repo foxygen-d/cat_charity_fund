4de1fec5bb8f2230821d78c6070cd1470db23bfc
Anastasia Domnina <foxygen.d@yandex.ru>
2023-02-06 00:28:06 +0300

    Ready

--- app/utils/investing.py
+++ app/utils/investing.py
@@ -13,12 +13,12 @@ async def get_not_full_invested_objects(
 ) -> List[Union[CharityProject, Donation]]:
     objects = await session.execute(
         select(obj_in).where(obj_in.fully_invested == 0
-        ).order_by(obj_in.create_date)
+                             ).order_by(obj_in.create_date)
     )
     return objects.scalars().all()
 
 
-async def close_donation_for_obj(obj_in: Union[CharityProject, Donation]) -> None:
+async def close_donation_for_obj(obj_in: Union[CharityProject, Donation]):
     obj_in.invested_amount = obj_in.full_amount
     obj_in.fully_invested = True
     obj_in.close_date = datetime.now()
@@ -29,19 +29,19 @@ async def money_invest(
     obj_in: Union[CharityProject, Donation],
     obj_model: Union[CharityProject, Donation],
 ) -> Union[CharityProject, Donation]:
-    available_amount_in = obj_in.full_amount - obj_in.invested_amount
-    available_amount_model = obj_model.full_amount - obj_model.invested_amount
+    free_amount_in = obj_in.full_amount - obj_in.invested_amount
+    free_amount_in_model = obj_model.full_amount - obj_model.invested_amount
 
-    if available_amount_in > available_amount_model:
-        obj_in.invested_amount += available_amount_model
+    if free_amount_in > free_amount_in_model:
+        obj_in.invested_amount += free_amount_in_model
         await close_donation_for_obj(obj_model)
 
-    elif available_amount_in == available_amount_model:
+    elif free_amount_in == free_amount_in_model:
         await close_donation_for_obj(obj_in)
         await close_donation_for_obj(obj_model)
 
     else:
-        obj_model.invested_amount += available_amount_in
+        obj_model.invested_amount += free_amount_in
         await close_donation_for_obj(obj_in)
 
     return obj_in, obj_model
